11df67e71c3cc838b53a57ed0299e085d766d04b
MuxDD11 <carlos.ene@edu.uah.es>
2024-06-11 09:36:08 +0300

    1
    First trial, some basic stuff

---
+++ from ase.py
@@ -0,0 +1,14 @@
+#       ase build -x diamond Si
+#       ase convert Si.json database.db
+#inspect database
+#       ase db database.db
+from ase.db import connect
+
+#db = connect('database.db')s
+#for row in db.select():
+#    atoms = row.toatoms()
+ #   print(atoms)
+
+db = connect('database.db')
+row = db.select(id=1)[0]
+dir(row)
